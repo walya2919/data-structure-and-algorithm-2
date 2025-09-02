bbd0d630b4cf583b0fad06b7aacdd3d325fbd6c5
Lee SJ Desktop <walya2919@gmail.com>
2025-09-03 03:45:34 +0900

    기숙사에서 조금 한거

--- insertion_sort.py
+++ insertion_sort.py
@@ -17,9 +17,18 @@ def insertion_sort(arr:list)->list:
     >> print(insertion_sort(arr))
     [1, 2, 3, 4, 5, 6]
     '''
-    for i in range(1, len(arr)):
+    sorted_arr = arr[:]
+    for i in range(1, len(sorted_arr)):
         for j in range(i-1, -1, -1):
             # i is target index and, j is 
-            if arr[i] >= arr[j]:
-                temp = arr[i]
-                arr[]
+            if sorted_arr[i] >= sorted_arr[j]:
+                # j+1 - i-1 => j+2 - i
+                # i goto j+1
+                temp = sorted_arr[i]
+                for k in range(i, j, -1):
+                    sorted_arr[k] = arr[k-1]
+                arr[j+1] = temp
+                break
+
+array = [1, 3, 5, 2, 23, 526]
+print(insertion_sort(array))
