47f8dc20527c551b400bdb17957f86cbd1774ea4
Lee SJ Desktop <walya2919@gmail.com>
2025-09-04 23:59:39 +0900

    주석 추가

--- sort_codes/merge_sort.py
+++ sort_codes/merge_sort.py
@@ -1,5 +1,24 @@
 def merge_sort(arr:list)->list:
+    '''Sort the array in ascending order with merge sort.
+
+    Parameters
+    ----------
+    arr : list
+        list to sort
+    
+    Returns
+    -------
+    sorted_arr : list
+        list has been sorted
+    
+    Example
+    -------
+    >> arr = [3, 6, 1, 2, 5, 4]
+    >> print(merge_sort(arr))
+    [1, 2, 3, 4, 5, 6]
+    '''
     if len(arr) > 2:
+        # seperate given array to two parts, and sort by using recursion
         left_arr = arr[:len(arr)//2]
         right_arr = arr[len(arr)//2:]
         sorted_left_arr = merge_sort(left_arr)
@@ -9,6 +28,7 @@ def merge_sort(arr:list)->list:
         left_idx, right_idx, merged_idx = 0, 0, 0
         left_len, right_len = len(left_arr), len(right_arr)
 
+        # finish at left or right index is reach end of array
         while left_idx < left_len and right_idx < right_len:
             if sorted_left_arr[left_idx] < sorted_right_arr[right_idx]:
                 merged_arr[merged_idx] = sorted_left_arr[left_idx]
@@ -17,7 +37,7 @@ def merge_sort(arr:list)->list:
                 merged_arr[merged_idx] = sorted_right_arr[right_idx]
                 right_idx += 1
             merged_idx += 1
-
+        # add rest array's elements to merged array
         if left_idx == left_len:
             merged_arr[merged_idx:] = sorted_right_arr[right_idx:]
         elif right_idx == right_len:
@@ -26,15 +46,16 @@ def merge_sort(arr:list)->list:
             exit(2)
 
         return merged_arr
-    
+    # compare each array and return sorted array
     elif len(arr) == 2:
         if arr[0] <= arr[1]:
             return arr[:]
         else:
             return arr[::-1]
-        
+    # if array's lenth is 1, return itself
     elif len(arr) == 1:
         return arr[:]
     
     else:
         exit(1)
+        
